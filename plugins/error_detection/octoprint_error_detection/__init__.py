906e06aa3eb40d0df27552fd61dc1dbea74a14e8
Darya Igonina <daria.vitaly@gmail.com>
2025-02-13 10:59:15 +0100

    renamed __init__v2 to __init__ for test purposes. old __init__ is now __init__old

--- plugins/error_detection/octoprint_error_detection/__init__.py
+++ plugins/error_detection/octoprint_error_detection/__init__.py
@@ -1,78 +1,112 @@
 # coding=utf-8
 from __future__ import absolute_import
-#from <ai_file> import <name_of_trained_ai_model>
-
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
 
 import octoprint.plugin
+import time
+import threading
+import logging
+import octoprint_error_detection.ai_model as ai_model  
+import plugins.error_detection.octoprint_error_detection.capture_image as capture_image
 
-class Error_detectionPlugin(
-    octoprint.plugin.StartupPlugin,
-    octoprint.plugin.SettingsPlugin,
+# initial implementation of the plugin is imported beneath
+# import plugins.error_detection.octoprint_error_detection.print as plugin
+
+class AIErrorDetectionPlugin(
+    # base plugin class
+    octoprint.plugin.OctoPrintPlugin,
+    # for UI purposes, for embedding JS and CSS
     octoprint.plugin.AssetPlugin,
-    octoprint.plugin.TemplatePlugin
-):
+    # for UI purposes, for plugin to be able to modify webinterface
+    octoprint.plugin.TemplatePlugin,
+    # for adding starting up functionality
+    octoprint.plugin.StartupPlugin,
+    # for adding shutting down functionality
+    octoprint.plugin.ShutdownPlugin):
 
     def on_after_startup(self):
-        self._logger.info("Hello World!")
+        self._logger.info("Hello World! I am the plugin")
 
-    ##~~ SettingsPlugin mixin
+    def initialize(self):
+        # initialize model
+        model_path = "model_weights\train_100_epochs\best.pt"
+        self.error_model = ai_model(model_path)  # Error detection model
+        self._monitoring = False  # Monitoring status
+            # self._logger.info("Plugin initialized.")
 
-    def get_settings_defaults(self):
-        return {
-            # put your plugin's default settings here
-        }
+        # Configure the logger
+        self._logger = self._logger
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self._logger.addHandler(handler)
+        self._logger.setLevel(logging.INFO)
 
-    ##~~ Softwareupdate hook
+        self._logger.info("Plugin initialized.")
 
+    # retrieve the UI functionality    
+    def get_assets(self):
+        return dict(
+        js=['plugins\error_detection\octoprint_error_detection\static\js\error_detection.js',
+            'plugins\error_detection\octoprint_error_detection\static\js\error_detection_v2.js'],
+        # clientjs=['clientjs/my_file.js'],
+        # css=['css/my_styles.css'],
+        # less=['less/my_styles.less']
+        )
+    
+    # create a tab in the octoprint webinterface
+    def get_template_configs(self):
+        return dict(type="tab", 
+                    template="plugins\error_detection\octoprint_error_detection\templates\tab_error_detection_dashboard.jinja2")
+    
     def get_update_information(self):
-        # Define the configuration for your plugin to use with the Software Update
-        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-        # for details.
-        return {
-            "error_detection": {
-                "displayName": "Error_detection Plugin",
-                "displayVersion": self._plugin_version,
+        # Return None or an empty dictionary to disable update checks
+        return {}
+    
+    """Check if the monitoring process started"""
+    def check_monitoring(self):
+        if self._monitoring:
+            self._monitoring = False
+            self._logger.info("Monitoring stopped")
+        else:
+            self._monitoring - True
+            threading.Thread(target=self.monitor_print, daemon=True).start()
+            self._logger.info("Monitoring started")
 
-                # version check: github repository
-                "type": "github_release",
-                "user": "Katja909",
-                "repo": "error_detection_with_AI",
-                "current": self._plugin_version,
+    def __plugin_load__(): 
+        global __plugin_implementation__
+        __plugin_implementation__ = AIErrorDetectionPlugin()
 
-                # update method: pip
-                "pip": "https://github.com/Katja909/error_detection_with_AI/archive/{target_version}.zip",
-            }
+        global __plugin_hooks__
+        __plugin_hooks__ = {
+            "octoprint.plugin.softwareupdate.check_config":
+            __plugin_implementation__.get_update_information
         }
+    
 
+    def monitor_print(self):
+        """Continuously monitors the print process for errors."""
+        while self._monitoring:
+            try:
+                # Example: Get the image from the printer camera (use your method to capture the print image)
+                # image = self.get_print_image()
+                image = capture_image.get_print_image()
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+                # Detect error in the captured image
+                if self.error_model.detect_error(image):
+                    self._logger.warning("Error detected in the print process!")
+                    self.notify_user("Error detected!")
+                    self._monitoring = False  # Stop monitoring
+                    self._printer.cancel_print()  # Cancel the print
+            except Exception as e:
+                self._logger.error(f"Error in monitoring process: {e}")
+            time.sleep(1)  # Adjustable monitoring frequency
+
+    def notify_user(self, message):
+        """Sends a notification to the user."""
+        # TODO: send_plugin_message method may not be initialized
+        self._plugin_manager.send_plugin_message(self._identifier, dict(type="error", message=message))
 
 __plugin_name__ = "Error Detection for 3D Printer with AI Model"
-__plugin_version__ = "0.1.0"
+__plugin_version__ = "0.1.1"
 __plugin_description__ = "An OctoPrint plugin for detecting errors during printing with help of an AI Model"
-
-
-# Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
-# OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
-# OctoPrint 1.8.0 onwards only supports Python 3.
-__plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
-
-def __plugin_load__():
-    global __plugin_implementation__
-    __plugin_implementation__ = Error_detectionPlugin()
-
-    global __plugin_hooks__
-    __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-    }
-
-# TODO: https://docs.octoprint.org/en/master/plugins/concepts.html#:~:text=When%20a%20plugin%20gets%20enabled%2C%20OctoPrint%20will%20also%20call%20the%20on_plugin_enabled()%20callback%20on%20its%20implementation%20(if%20it%20exists).%20Likewise%2C%20when%20a%20plugin%20gets%20disabled%20OctoPrint%20will%20call%20the%20on_plugin_disabled()%20callback%20on%20its%20implementation%20(again%2C%20if%20it%20exists).
+__plugin_implementation__ = AIErrorDetectionPlugin()
