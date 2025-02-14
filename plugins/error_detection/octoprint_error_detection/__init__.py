39538dad4857d6dae136f02aef58ccc270544c05
Darya Igonina <daria.vitaly@gmail.com>
2025-02-14 16:13:46 +0100

    debugging, loading in octoprint not working

--- plugins/error_detection/octoprint_error_detection/__init__.py
+++ plugins/error_detection/octoprint_error_detection/__init__.py
@@ -11,41 +11,41 @@ from .print import MyPlugin
 #
 # Take a look at the documentation on what other plugin mixins are available.
 
-import octoprint.plugin
+# import octoprint.plugin
 
-class Error_detectionPlugin(octoprint.plugin.SettingsPlugin,
-    octoprint.plugin.AssetPlugin,
-    octoprint.plugin.TemplatePlugin
-):
+# class Error_detectionPlugin(octoprint.plugin.SettingsPlugin,
+#     octoprint.plugin.AssetPlugin,
+#     octoprint.plugin.TemplatePlugin
+# ):
 
-    ##~~ SettingsPlugin mixin
+#     ##~~ SettingsPlugin mixin
 
-    def get_settings_defaults(self):
-        return {
-            # put your plugin's default settings here
-        }
+#     def get_settings_defaults(self):
+#         return {
+#             # put your plugin's default settings here
+#         }
 
-    ##~~ Softwareupdate hook
+#     ##~~ Softwareupdate hook
 
-    def get_update_information(self):
-        # Define the configuration for your plugin to use with the Software Update
-        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-        # for details.
-        return {
-            "error_detection": {
-                "displayName": "Error_detection Plugin",
-                "displayVersion": self._plugin_version,
+#     def get_update_information(self):
+#         # Define the configuration for your plugin to use with the Software Update
+#         # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+#         # for details.
+#         return {
+#             "error_detection": {
+#                 "displayName": "Error_detection Plugin",
+#                 "displayVersion": self._plugin_version,
 
-                # version check: github repository
-                "type": "github_release",
-                "user": "Katja909",
-                "repo": "error_detection_with_AI",
-                "current": self._plugin_version,
+#                 # version check: github repository
+#                 "type": "github_release",
+#                 "user": "Katja909",
+#                 "repo": "error_detection_with_AI",
+#                 "current": self._plugin_version,
 
-                # update method: pip
-                "pip": "https://github.com/Katja909/error_detection_with_AI/archive/{target_version}.zip",
-            }
-        }
+#                 # update method: pip
+#                 "pip": "https://github.com/Katja909/error_detection_with_AI/archive/{target_version}.zip",
+#             }
+#         }
 
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
@@ -55,18 +55,22 @@ class Error_detectionPlugin(octoprint.plugin.SettingsPlugin,
 __plugin_name__ = "Error Detection for 3D Printer with AI Model"
 __plugin_version__ = "0.1.0"
 __plugin_description__ = "An OctoPrint plugin for detecting errors during printing with help of an AI Model"
+__plugin_implementation__ = MyPlugin()
+__plugin_hooks__ = {
+    "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+}
+__plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
 
 
 # Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
 # OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
 # OctoPrint 1.8.0 onwards only supports Python 3.
-__plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
 
-def __plugin_load__():
-    global __plugin_implementation__
-    __plugin_implementation__ = MyPlugin()
+# def __plugin_load__():
+#     global __plugin_implementation__
+#     __plugin_implementation__ = MyPlugin()
 
-    global __plugin_hooks__
-    __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-    }
+#     global __plugin_hooks__
+#     __plugin_hooks__ = {
+#         "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+#     }
