24106d32bc3dede7fb3cb5ff3b0f37b5b91646f8
Darya Igonina <daria.vitaly@gmail.com>
2025-02-19 15:45:03 +0100

    removed unnecessary logs

--- plugins/error_detection/octoprint_error_detection/ai_model.py
+++ plugins/error_detection/octoprint_error_detection/ai_model.py
@@ -94,7 +94,7 @@ class ai_model:
             logger.info("AI Model output generated.")
             for detection in detections[0]:
                 confidence = detection[4]
-                logger.info(f"AI Model: Confidence: {confidence * 100:.2f}%")
+                # logger.info(f"AI Model: Confidence: {confidence * 100:.2f}%")
                 if confidence * 100 > 5:
                     logger.info(f"AI Model: Detected error with confidence: {confidence * 100:.2f}%")
                     return True
